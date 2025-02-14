7bd8b22aeefe7276e93cdc52c1b26ef0b65f8f8b
Gabriel Alexandre <gabizinhoalexandre@gmail.com>
2025-02-14 11:58:39 +0000

    view do bilnhete

--- BeaverEnterprisesMVC/Controllers/HomeController.cs
+++ BeaverEnterprisesMVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace BeaverEnterprisesMVC.Controllers
             return View();
         }
 
+        public IActionResult BookingAvailability()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
