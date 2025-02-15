dc9da1dbcaec6e71f58de42d3e84562ea3b5ffb5
joão tomás <joaotomassilva55@gmail.com>
2025-02-15 13:51:04 +0000

    nao sei o que alterei

---
+++ BeaverEnterprisesMVC/Controllers/AirportController.cs
@@ -0,0 +1,34 @@
+﻿using BeaverEnterprisesMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BeaverEnterprisesMVC.Controllers
+{
+    public class AirportController : Controller
+    {
+
+        
+        BeaverEnterprisesContext entities = new BeaverEnterprisesContext();
+
+        [HttpGet]
+        public JsonResult GetSuggestions(string term)
+        {
+            try
+            {
+                // Consultar os dados e projetar apenas o campo Name
+                var resultados = entities.Locations
+                    .Where(a => a.Name.ToLower().Contains(term.ToLower()))
+                    .Select(a => a.Name) // Selecionando apenas o Name
+                    .ToList();
+
+                // Retornando a lista de nomes em formato JSON
+                return Json(resultados);
+            }
+            catch (Exception ex)
+            {
+                // Caso ocorra algum erro, retornamos uma mensagem de erro
+                return Json(new { error = "Ocorreu um erro ao processar a solicitação", message = ex.Message });
+            }
+        }
+    }
+}
--- BeaverEnterprisesMVC/Models/BeaverEnterprisesContext.cs
+++ BeaverEnterprisesMVC/Models/BeaverEnterprisesContext.cs
@@ -35,7 +35,7 @@ public partial class BeaverEnterprisesContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=ROTOM;Database=BeaverEnterprises;Trusted_Connection=True;Persist Security Info=True;TrustServerCertificate=True;MultipleActiveResultSets=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-N4C79H0;Database=BeaverEnterprises;Trusted_Connection=True;Persist Security Info=True;TrustServerCertificate=True;MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
